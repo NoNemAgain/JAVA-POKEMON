cd4c51a9732a13d0e5225368590d62f0d70b0d58
NoNemAgain <NoNemAgain@users.noreply.github.com>
2019-12-10 15:58:53 +0100

    10_12

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Pokedex.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Pokedex.java
@@ -10,25 +10,25 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class Pokedex {
-	private HashMap<Integer,Pokemon> pokedex;
-	private static final String fileName = "src/fr/upem/dut/info/source/pokedex.csv";
-	private final int version ;
-	public Pokedex () throws IOException {
-		Path file = Paths.get(fileName);
-		Charset charset = StandardCharsets.UTF_8;
-		HashMap<Integer,Pokemon> pokedex = new HashMap<Integer,Pokemon>();
-		int i = 0;
-		try (BufferedReader reader = Files.newBufferedReader(file, charset)){
-			String[] line;
-			while ((line = reader.readLine().split(",")) != null) {
-				if(i !=0) {
-					pokedex.put(line[0], new Pokemon(Integer.parseInt(line[0]), line[1], line[2], Integer.parseInt(line[3]), Integer.parseInt(line[4]), Type.line[5]))
-				}
-				i++;
-			}
-		}
-		this.version=0;
-	}
+    private HashMap<Integer,Pokemon> pokedex;
+    private static final String fileName = "src/fr/upem/dut/info/source/pokedex.csv";
+    private final int version ;
+    public Pokedex () throws IOException {
+        Path file = Paths.get(fileName);
+        Charset charset = StandardCharsets.UTF_8;
+        HashMap<Integer,Pokemon> pokedex = new HashMap<Integer,Pokemon>();
+        int i = 0;
+        try (BufferedReader reader = Files.newBufferedReader(file, charset)){
+            String[] line;
+            while ((line = reader.readLine().split(",")) != null) {
+                if(i !=0) {
+                    pokedex.put(Integer.parseInt(line[0]), new Pokemon(Integer.parseInt(line[0]), line[1], line[2], Integer.parseInt(line[3]), Integer.parseInt(line[4]),  Type.getType((line[5]))));
+                }
+                i++;
+            }
+        }
+        this.version=0;
+    }
 
-	
+    
 }
--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Type.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Type.java
@@ -1,22 +1,33 @@
 package fr.upem.dut.info.pokemonproject;
 
 public enum Type {
-	normal, 
-	grass, 
-	poison, 
-	fire, 
-	flying, 
-	water, 
-	bug, 
-	electric, 
-	ground, 
-	fairy, 
-	psychic, 
-	rock, 
-	steel, 
-	ice, 
-	ghost, 
-	dragon, 
-	dark ,
-	fighting ;
+    normal, 
+    grass, 
+    poison, 
+    fire, 
+    flying, 
+    water, 
+    bug, 
+    electric, 
+    ground, 
+    fairy, 
+    psychic, 
+    rock, 
+    steel, 
+    ice, 
+    ghost, 
+    dragon, 
+    dark ,
+    fighting ;
+    
+    static Type getType(String str) {//
+        if(str == "normal") {
+            return Type.normal;
+        }
+        else {
+            return Type.bug;
+        }
+            
+        
+    }
 }
