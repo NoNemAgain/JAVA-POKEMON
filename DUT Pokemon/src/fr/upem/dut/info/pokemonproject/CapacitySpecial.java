9e2f0045ec4dba37ef58fafca86da861f454d62f
NoNemAgain <NoNemAgain@users.noreply.github.com>
2019-12-10 17:27:06 +0100

    1727 101219

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/CapacitySpecial.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/CapacitySpecial.java
@@ -2,11 +2,8 @@ package fr.upem.dut.info.pokemonproject;
 
 public class CapacitySpecial extends AbstractCapacity{
 
-    public CapacitySpecial(String name, int power, int precision, int limit, Type type, Type type2) {
-        super(name, power, precision, limit, type, type2);
-    }
-    public CapacitySpecial(String name, int power, int precision,int limit, Type type) {
-        this(name,power,precision,limit, type, null);
+    public CapacitySpecial(String name, int power, int precision, int limit, Type type) {
+        super(name, power, precision, limit, type);
     }
     public void damage(PokemonFight p) {
     	super.damage(p);
