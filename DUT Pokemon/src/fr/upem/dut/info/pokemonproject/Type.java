aafac80e61cf59481baa7b6819a6b394f63e3238
natha <natha@192.168.1.64>
2019-12-10 16:02:22 +0100

    type 16:02  10_12_2019

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Type.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Type.java
@@ -20,12 +20,63 @@ public enum Type {
 	dark ,
 	fighting ;
 	
-	static Type getType(String str) {//
+	public static Type getType(String str) {
 		if(str == "normal") {
 			return Type.normal;
 		}
-		else {
+		if(str == "grass") {
+			return Type.grass;
+		}
+		if(str == "poison") {
+			return Type.poison;
+		}
+		if(str == "fire") {
+			return Type.fire;
+		}
+		if(str == "flying") {
+			return Type.flying;
+		}
+		if(str == "water") {
+			return Type.water;
+		}
+		if(str == "bug") {
 			return Type.bug;
+		}
+		if(str == "electric") {
+			return Type.electric;
+		}
+		if(str == "ground") {
+			return Type.ground;
+		}
+		if(str == "fairy") {
+			return Type.fairy;
+		}
+		if(str == "psychic") {
+			return Type.psychic;
+		}
+		if(str == "rock") {
+			return Type.rock;
+		}
+		if(str == "steel") {
+			return Type.steel;
+		}
+		if(str == "ice") {
+			return Type.ice;
+		}
+		if(str == "ghost") {
+			return Type.ghost;
+		}
+		if(str == "dragon") {
+			return Type.dragon;
+		}
+		if(str == "dark") {
+			return Type.dark;
+		}
+		if(str == "fighting") {
+			return Type.fighting;
+		}
+		else {
+			return null;
 		}
 			
 		
