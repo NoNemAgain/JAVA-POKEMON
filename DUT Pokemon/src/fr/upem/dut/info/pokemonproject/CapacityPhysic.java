ebc25b2b36ebe7b3c2074feb3b1cd15918b82e7f
NoNemAgain <NoNemAgain@users.noreply.github.com>
2019-12-10 14:52:04 +0100

    14:52

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/CapacityPhysic.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/CapacityPhysic.java
@@ -1,6 +1,7 @@
 package fr.upem.dut.info.pokemonproject;
 
 public class CapacityPhysic extends AbstractCapacity{
+<<<<<<< Upstream, based on origin/master
 	public CapacityPhysic(String name, int power, int precision, int limit, Type type, Type type2) {
 		super(name, power, precision, limit, type, type2);
 	}
@@ -8,3 +9,12 @@ public class CapacityPhysic extends AbstractCapacity{
 		this(name,power,precision,limit, type, null);
 	}
 }
+=======
+    public CapacityPhysic(String name, int power, int precision, int limit, Type type, Type type2) {
+        super(name, power, precision, limit, type, type2);
+    }
+    public CapacityPhysic(String name, int power, int precision,int limit, Type type) {
+        this(name,power,precision,limit, type, null);
+    }
+}
+>>>>>>> f57f4e3 14:52
--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Pokedex.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/Pokedex.java
@@ -1,14 +1,23 @@
 package fr.upem.dut.info.pokemonproject;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 public class Pokedex {
 	private HashMap<Integer,Pokemon> pokedex;
+	 private static final String fileName = "src/fr/upem/dut/info/source/pokedex.csv";
 	private final int version ;
 	public Pokedex () {
 		HashMap<Integer,Pokemon> pokedex = new HashMap<>();
 		this.version=0;
 	}
+	public void init() throws FileNotFoundException {
+		FileReader fr = new FileReader(fileName);
+        BufferedReader br = new BufferedReader(fr);
+	}
+	
 	
 
 }
