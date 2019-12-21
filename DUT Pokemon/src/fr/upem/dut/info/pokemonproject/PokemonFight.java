be5952f62d04a3ab9097259387997deeae998aad
natha <natha@192.168.1.64>
2019-12-21 15:07:54 +0100

    equals and hashcode

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/PokemonFight.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/PokemonFight.java
@@ -43,7 +43,7 @@ public class PokemonFight extends Pokemon{
 		}
 		PokemonFight p  = (PokemonFight) o;
 		return super.equals(p)&&capacities.equals(p.capacities);
-		}
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(super.hashCode(),capacities);
