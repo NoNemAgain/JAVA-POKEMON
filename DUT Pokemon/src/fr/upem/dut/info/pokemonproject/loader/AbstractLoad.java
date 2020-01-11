4edbc3ca483a8901283921aff93106300abdb439
NathanSaucet <nathans77340@gmail.com>
2020-01-11 19:43:58 +0100

    dzad

--- DUT Pokemon/src/fr/upem/dut/info/pokemonproject/loader/AbstractLoad.java
+++ DUT Pokemon/src/fr/upem/dut/info/pokemonproject/loader/AbstractLoad.java
@@ -8,15 +8,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import fr.upem.dut.info.pokemonproject.pokemon.PokemonFight;
-
 abstract class AbstractLoad {
 	/**
 	 *  This class permit to pass on the function Create to {@link PokeCapacity}, {@link Pokedex}, {@link TypesMultiplicators}
 	 */
 	public AbstractLoad() throws IOException {
 	}
-
 	// load the file
 	protected void create(String fileName) throws IOException {
 		Path file = Paths.get(fileName);
@@ -34,7 +31,5 @@ abstract class AbstractLoad {
 		}
 
 	}
-
 	abstract void add(String[] line);
-
 }
